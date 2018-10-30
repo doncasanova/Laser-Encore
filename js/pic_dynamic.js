dc8530ec70e5b88a4b61c057ada7fc3f86c27907
doncasanova <ethanjuan@yahoo.com>
2018-10-30 18:21:17 -0500

    d

---
+++ js/pic_dynamic.js
@@ -0,0 +1,69 @@
+
+
+var image = ["https://i1.wp.com/laserencore.com/wp-content/uploads/2017/02/edmlasers4.jpg?fit=930%2C532&ssl=1", "https://i0.wp.com/laserencore.com/wp-content/uploads/2017/02/indoorbeams.jpg?fit=930%2C523&ssl=1", "https://i2.wp.com/laserencore.com/wp-content/uploads/2017/02/waterscreen2.jpg?fit=930%2C932&ssl=1", "https://i2.wp.com/laserencore.com/wp-content/uploads/2017/02/schweitzerlasers.jpg?fit=930%2C621&ssl=1", "https://i1.wp.com/laserencore.com/wp-content/uploads/2017/02/laserbillboard.jpg?fit=663%2C997&ssl=1"];
+//const imagePath = "./img/test/";
+addImage();
+var imageNumber = 0;
+var imageModal = 0;
+function addImage() {
+    for (i = 0; i < image.length; i++) {
+
+        $(".insertImagesHere").append(`<div id = "${image[i]}" class="imageContainer"><a href="#" class="" data-toggle="modal" data-target="#largeModal"><img class="imageStyle imageHover pointer"src= "${image[i]}" alt="${image[i]}"></a></div>`);
+        var imageNumber = i;
+
+    }
+
+}
+
+const newLocal = ".imageStyle";
+
+$(newLocal).on("click", function () {
+    $(".insertImagesModal").empty();
+    $("#myModalLabel").empty();
+
+    var imageModal = $(this).attr('src');
+    var imageModalName = imageModal.substr(11).slice(0, -4);
+    $(".insertImagesModal").append(`<img class="modalImageStyle"src= "${imageModal}" alt="${imageModal}">`);
+    $("#myModalLabel").append(`<h4 id="myModalLabel" class="modal-title" >${imageModalName}</h4>`);
+});
+
+
+
+//special events start
+
+
+
+//var image = ["dixie.jpg", "dixie on a bird.jpg", "food turkey 068.JPG", "skiing.jpg", "SnarlingWolf.jpg"];
+//const imagePath = "./img/test/";
+addSpecialEventImage();
+//var imageNumber = 0;
+//var imageModal = 0;
+function addSpecialEventImage() {
+    for (i = 0; i < image.length; i++) {
+
+        $(".specialEvent").append(`<div class="carousel-item active imageButton">
+    <a href="#" class="" data-toggle="modal" data-target="#largeModal"><img class="slider2 imageModal" src= "${image[i]}" alt="${image[i]} style="width:350px;height:300px;" title="EDM Lasers"></a>
+        <a href="specialEvents.html" class="btn btn-info" role="button">EDM Lasers</a>
+                        </div>`);
+        //var imageNumber = i;
+
+    }
+
+}
+
+const modalOne = ".imageModal";
+
+$(modalOne).on("click", function () {
+    $(".insertImagesModal").empty();
+    $("#myModalLabel").empty();
+
+    var imageModal = $(this).attr('src');
+    console.log(imageModal);
+    var imageModalName = imageModal.substr(11).slice(0, -4);
+    $(".insertImagesModal").append(`<img class="modalImageStyle"src= "${imageModal}" alt="${imageModal}">`);
+    $("#myModalLabel").append(`<h4 id="myModalLabel" class="modal-title" >EDM Lasers</h4>`);
+});
+
+
+
+// special events end
